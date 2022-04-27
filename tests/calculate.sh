b57116af748911658fd26e5092c2b26119373d93
Felix Beaudry <felix.beaudry@gmail.com>
2022-04-27 11:33:07 -0400

    created vidarr testing suite

--- tests/calculate.sh
+++ tests/calculate.sh
@@ -5,6 +5,5 @@ set -o pipefail
 
 cd $1
 
-find . -regex '.*\.sigtools.hrd.txt$' -exec md5sum {} \;
-find . -regex '.*\.sigtools.model.txt$' -exec md5sum {} \;
-find . -regex '.*\.sigtools.sigs.txt$' -exec md5sum {} \;
+find . -regex '.*\.msi$' -exec md5sum {} \;
+
